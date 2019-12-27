847d3001df78cdfaefe61d52025b7119de6a2fe4
ituitis-bozkurtsen19 <56247446+ituitis-bozkurtsen19@users.noreply.github.com>
2019-12-27 20:12:22 +0300

    final changes added

--- server.py
+++ server.py
@@ -1,6 +1,12 @@
 import os
 from bottle import Bottle, request, template, static_file
 from pathlib import Path
+from hashlib import sha256
+
+
+def create_hash(password):
+    pw_bytestring = password.encode()
+    return sha256(pw_bytestring).hexdigest()
 
 
 ip_dict = {}
@@ -11,8 +17,21 @@ def hello():
     if client_ip in ip_dict:
         ip_dict[client_ip] += 1
     else:
-        ip_dict[client_ip] = 1
+        ip_dict.update({client_ip:1})
     return template("index.html", ippp=ip_dict)
+
+
+def resetter(filename):
+    incoming_pwd = request.forms.password
+    terms = request.forms.terms
+    hashed_entry = create_hash(incoming_pwd)
+    point = request.forms.get
+    while True:
+        if hashed_entry == "8e20775010852c6e583e1d0510b50644011ebca66e6930cd14d848926a39070a" and terms == "accepted":
+            ip_dict.clear()
+            return template('reset.html')
+        else:
+            return template("wrongpwd.html")
 
 
 def html_statics(file_name):
@@ -28,6 +47,7 @@ def create_app():
     app.route("/", "GET", hello)
     app.route("/<file_name:path>", "GET", html_statics)
     app.route("/<file>", "GET", reader)
+    app.route('/<filename>', 'POST', resetter)
     return app
 
 
